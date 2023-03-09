d15289ae1c79f943901e34ec7cb62c75ce32a766
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-03-08 22:10:39 -0500

    Added a code to recursively add folders, subfolders and files to the TODO

--- src/main.rs
+++ src/main.rs
@@ -23,3 +23,43 @@ fn main() {
         println!("Counter is: {}", args.count);
     }
 }
+
+// TODO: Copy recursively the folders and files
+// use std::fs;
+// use std::io::{self, ErrorKind};
+// use std::path::{Path, PathBuf};
+
+// fn copy_dir_recursively(src: &Path, dest: &Path) -> io::Result<()> {
+//     if !src.is_dir() {
+//         return Err(io::Error::new(ErrorKind::Other, "Source path is not a directory"));
+//     }
+
+//     if !dest.exists() {
+//         fs::create_dir_all(dest)?;
+//     }
+
+//     for entry in fs::read_dir(src)? {
+//         let entry = entry?;
+//         let path = entry.path();
+//         let file_name = path.file_name().unwrap().to_owned();
+//         let dest_path = dest.join(&file_name);
+
+//         if path.is_dir() {
+//             copy_dir_recursively(&path, &dest_path)?;
+//         } else {
+//             fs::copy(&path, &dest_path)?;
+//         }
+//     }
+
+//     Ok(())
+// }
+
+// fn main() -> io::Result<()> {
+//     let src = Path::new("path/to/source/folder");
+//     let dest = Path::new("path/to/destination/folder");
+
+//     copy_dir_recursively(&src, &dest)?;
+
+//     println!("Folder copied successfully!");
+//     Ok(())
+// }
